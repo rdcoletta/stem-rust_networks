a0d37b9e539ef9762c79657e2f93be39e29abff0
rdcoletta <della028@umn.edu>
2020-10-01 10:34:07 -0500

    added option to extract gene correlations from entire network

--- scripts/coexpression_scores.py
+++ scripts/coexpression_scores.py
@@ -14,7 +14,8 @@ description: this script extracts co-expression scores between genes of a
 parser.add_argument("network_name", type=str,
                     help="name of Camoco network")
 parser.add_argument("cluster_list", type=str,
-                    help="comma-separated list of Camoco clusters")
+                    help="comma-separated list of Camoco clusters, or write "
+                    "'all' to return correlations for entire network")
 parser.add_argument("output_prefix", type=str,
                     help="prefix of output name")
 # pass arguments into variables
@@ -27,38 +28,47 @@ output_prefix = args.output_prefix
 # transform network into a COB object
 COB = co.COB(network_name)
 
-# get all gene ids in camoco network
-genes_network = COB.genes()
-
-# transform clusters into integers
-cluster_list = [int(cluster) for cluster in cluster_list.split(",")]
-
-for cluster in cluster_list:
-
-    print("Retrieving cluster", str(cluster))
-    # get all gene names in cluster
-    genes_cluster = COB.clusters.loc[COB.clusters["cluster"] == cluster]
-    # names are actually row names
-    genes_cluster = list(genes_cluster.index)
-
-    # the gene ids from networks are actually a class with different attributes
-    # (e.g. name, chromosome, start/end position, etc.), but the gene ids in the
-    # cluster shows only the name of the gene
-
-    # get full gene ids for all genes in cluster
-    genes_cluster = [gene for gene in genes_network if gene.name in genes_cluster]
-
+if cluster_list == "all":
     # get data frame with co-expression scores
-    scores_df = COB.subnetwork(genes_cluster, sig_only = True, names_as_cols = True)
+    scores_df = COB.subnetwork(sig_only = True, names_as_cols = True)
     # keep only important columns
     scores_df = scores_df[["gene_a", "gene_b", "score"]]
+    # export data frame data to tsv, GraphML and JSON formats
+    scores_df.to_csv(output_prefix + ".txt", sep = "\t", index = False)
+    COB.to_graphml(file = output_prefix + ".graphml")
+    COB.to_json(filename = output_prefix + ".json", sig_only = True)
 
-    # export data frame data to tsv
-    output_name = output_prefix + ".cluster_" + str(cluster) + ".txt"
-    scores_df.to_csv(output_name, sep = "\t", index = False)
+else:
+    # get all gene ids in camoco network
+    genes_network = COB.genes()
+    # transform clusters into integers
+    cluster_list = [int(cluster) for cluster in cluster_list.split(",")]
 
-    # also exports data to GraphML and JSON formats
-    COB.to_graphml(file = output_prefix + ".cluster_" + str(cluster) + ".graphml",
-                   gene_list = genes_cluster)
-    COB.to_json(filename = output_prefix + ".cluster_" + str(cluster) + ".json",
-                gene_list = genes_cluster, sig_only = True)
+    for cluster in cluster_list:
+
+        print("Retrieving cluster", str(cluster))
+        # get all gene names in cluster
+        genes_cluster = COB.clusters.loc[COB.clusters["cluster"] == cluster]
+        # names are actually row names
+        genes_cluster = list(genes_cluster.index)
+
+        # the gene ids from networks are actually a class with different attributes
+        # (e.g. name, chromosome, start/end position, etc.), but the gene ids in the
+        # cluster shows only the name of the gene
+
+        # get full gene ids for all genes in cluster
+        genes_cluster = [gene for gene in genes_network if gene.name in genes_cluster]
+        # get data frame with co-expression scores
+        scores_df = COB.subnetwork(genes_cluster, sig_only = True, names_as_cols = True)
+        # keep only important columns
+        scores_df = scores_df[["gene_a", "gene_b", "score"]]
+
+        # export data frame data to tsv
+        output_name = output_prefix + ".cluster_" + str(cluster) + ".txt"
+        scores_df.to_csv(output_name, sep = "\t", index = False)
+
+        # also exports data to GraphML and JSON formats
+        COB.to_graphml(file = output_prefix + ".cluster_" + str(cluster) + ".graphml",
+                       gene_list = genes_cluster)
+        COB.to_json(filename = output_prefix + ".cluster_" + str(cluster) + ".json",
+                    gene_list = genes_cluster, sig_only = True)
